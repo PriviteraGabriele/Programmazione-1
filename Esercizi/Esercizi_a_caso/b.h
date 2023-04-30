3492d4303b8e01fc0671ef2f30a398ac21373ebc
Gabriele Privitera <102214867+PriviteraGabriele@users.noreply.github.com>
2023-04-30 18:21:48 +0200

    Delete Esercizi/Esercizi_a_caso directory

--- Esercizi/Esercizi_a_caso/b.h
+++
@@ -1,6 +0,0 @@
-/* Scrivere una funzione che prenda in input una matrice quadrata A 
-di short, e restituisca un bool che indichi se la diagonale principale è 
-identica (procedendo dall’alto verso il basso) alla secondaria. */
-
-
-bool identica(short A[n][n]);
